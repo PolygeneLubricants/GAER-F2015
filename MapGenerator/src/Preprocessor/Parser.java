53e8f216cb59eb60c6b32b98723f112fb4f8aa60
Andreas R. L. Christiansen <AndreasRLC@gmail.com>
2015-03-23 12:12:35 +0100

    Added visual test

--- MapGenerator/src/Preprocessor/Parser.java
+++ MapGenerator/src/Preprocessor/Parser.java
@@ -1,9 +1,40 @@
 package Preprocessor;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.ShortBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * User: AndreasRydingLund
  * Date: 23-03-2015
  */
 public class Parser {
+    public short[][] read(String filePath) throws IOException {
+        FileChannel fc;
+        fc = new FileInputStream(filePath).getChannel();
 
+        ByteBuffer bb = ByteBuffer.allocateDirect((int) fc.size());
+        while (bb.remaining() > 0) fc.read(bb);
+        fc.close();
+        bb.flip();
+
+        // Choose the right endianness
+        ShortBuffer sb = bb.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
+        short[][] matrix = new short[1201][];
+        int rowNumber = 0;
+        while (sb.remaining() > 0) {
+            short[] row = new short[1201];
+            for (int i = 0; i < 1201; i++) {
+                row[i] = sb.get();
+            }
+
+            matrix[rowNumber] = row;
+            rowNumber++;
+        }
+
+        return matrix;
+    }
 }
--- MapGenerator/src/TestSuite/Preprocessor/ParserTest.java
+++ MapGenerator/src/TestSuite/Preprocessor/ParserTest.java
@@ -1,6 +1,12 @@
 package TestSuite.Preprocessor;
 
+import Preprocessor.Parser;
 import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * User: AndreasRydingLund
@@ -8,7 +14,77 @@ import org.junit.Test;
  */
 public class ParserTest {
     @Test
-    public void TestParsing() {
+    public void testReading() {
+        Parser p = new Parser();
+        try {
+            p.read("D:/Users/Andreas/Git/GAER-F2015/MapGenerator/data/raw/N32/N52E006.hgt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void testVisualIdentification() {
+        Parser p = new Parser();
+        short[][] altitudeMap = new short[0][];
+        try {
+            altitudeMap = p.read("D:/Users/Andreas/Git/GAER-F2015/MapGenerator/data/raw/N32/N52E006.hgt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JFrame frame = new JFrame("Visual verification of SRTM maps");
+
+        PixelMap panel = new PixelMap(altitudeMap);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        while(true);
+    }
+
+    @Test
+    public void testSerialization() {
+
+    }
+
+    @Test
+    public void testDeSerialization() {
+
+    }
+
+    private class PixelMap extends JPanel {
+        private BufferedImage canvas;
+
+        public PixelMap(short[][] altitudeMap) {
+            canvas = new BufferedImage(altitudeMap[0].length, altitudeMap.length, BufferedImage.TYPE_INT_RGB);
+            fillCanvas(altitudeMap);
+        }
+
+        public Dimension getPreferredSize() {
+            return new Dimension(canvas.getWidth(), canvas.getHeight());
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawImage(canvas, null, null);
+        }
+
+
+        public void fillCanvas(short[][] altitudeMap) {
+            for (int x = 0; x < canvas.getWidth(); x++) {
+                for (int y = 0; y < canvas.getHeight(); y++) {
+                    int green = altitudeMap[x][y] > 0 ? (altitudeMap[x][y] * 255 / Short.MAX_VALUE) : 0;
+                    int blue = altitudeMap[x][y] < 0 ? ((altitudeMap[x][y] * -1 * 255) / Short.MAX_VALUE) : 0;
+
+                    Color c = new Color(0, green, blue);
+                    canvas.setRGB(x, y, c.getRGB());
+                }
+            }
+            repaint();
+        }
     }
 }
