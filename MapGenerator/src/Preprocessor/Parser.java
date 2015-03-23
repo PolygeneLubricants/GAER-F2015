98a8c938ee0abe616bf738d97f6cd6652ff2bcbe
Andreas R. L. Christiansen <AndreasRLC@gmail.com>
2015-03-23 10:12:28 +0100

    Created scaffold

---
+++ MapGenerator/src/Preprocessor/Parser.java
@@ -0,0 +1,9 @@
+package Preprocessor;
+
+/**
+ * User: AndreasRydingLund
+ * Date: 23-03-2015
+ */
+public class Parser {
+
+}
---
+++ MapGenerator/src/TestSuite/Preprocessor/ParserTest.java
@@ -0,0 +1,14 @@
+package TestSuite.Preprocessor;
+
+import org.junit.Test;
+
+/**
+ * User: AndreasRydingLund
+ * Date: 23-03-2015
+ */
+public class ParserTest {
+    @Test
+    public void TestParsing() {
+
+    }
+}
