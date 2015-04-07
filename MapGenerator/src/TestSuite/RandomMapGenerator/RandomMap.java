07473df11f7341b50908795da046b42d7a0ec4e6
Patrikk Sørensen <patri-kk@hotmail.com>
2015-04-07 13:33:21 +0200

    Created two functions, one for generating a random 3000x3000 altitudemap and a function for finding maxheight.

--- MapGenerator/src/TestSuite/RandomMapGenerator/RandomMap.java
+++ MapGenerator/src/TestSuite/RandomMapGenerator/RandomMap.java
@@ -7,17 +7,36 @@ import Preprocessor.Parser;
  */
 public class RandomMap {
     Parser p = new Parser();
-    short[][] matrix = new short[][]{
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-    };
+    short[][] matrix;
+
+    public static short[][] CreateRandomMap(){
+        short[][] randomMap = new short[3000][3000];
+        short initialMaxHeight = Short.MAX_VALUE;
+
+        for(int i = 0; i<randomMap.length; i++){
+            for(int j = 0; j< randomMap[i].length; j++){
+                randomMap[i][j] = (short) ((Math.random()-0.5)*initialMaxHeight);
+            }
+        }
+
+
+        return randomMap;
+    }
+
+    public static short FindMaxHeight(short[][] altitudeMap){
+        short maxHeight = 0;
+
+        for(int i = 0; i<altitudeMap.length; i++)
+            for(int j = 0; j< altitudeMap[i].length; j++)
+                if(altitudeMap[i][j] > maxHeight)
+                    maxHeight = altitudeMap[i][j];
+
+        return maxHeight;
+    }
 
     public void setMatrix(short[][] matrix) {
         this.matrix = matrix;
     }
-
-
 
     public short[][] getMatrix() {
         return matrix;
